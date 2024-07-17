b19025bf937e18d8c7c0e66b6408383c86df86fd
Washington Irving <iamwashingyou@gmail.com>
2024-07-18 00:34:07 +0300

    add resolution

---
+++ src/utils/resolution.rs
@@ -0,0 +1,26 @@
+pub struct Resolution {
+    pub width: u64,
+    pub height: u64,
+}
+
+impl Resolution {
+    pub fn new(width: u64, height: u64) -> Resolution {
+        Resolution { width, height }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_WIDTH: u64 = 1920;
+    const TEST_HEIGHT: u64 = 1080;
+
+    #[test]
+    fn test_new() {
+        let resolution = Resolution::new(TEST_WIDTH, TEST_HEIGHT);
+
+        assert_eq!(resolution.width, TEST_WIDTH);
+        assert_eq!(resolution.height, TEST_HEIGHT);
+    }
+}
