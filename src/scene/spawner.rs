a4d7492f0a7c0e479b129b33cbde403b2a757e6b
Washington Irving <iamwashingyou@gmail.com>
2024-07-18 23:31:02 +0300

    add spawner tests

--- src/scene/spawner.rs
+++ src/scene/spawner.rs
@@ -1,5 +1,6 @@
 use crate::{components::ComponentsPayload, Entity};
 
+/// Spawner is used by entity behaviors to add or remove entities in the scene
 pub struct Spawner {
     pub(crate) entities_to_add: Vec<ComponentsPayload>,
     pub(crate) entities_to_remove: Vec<Entity>,
@@ -25,5 +26,31 @@ impl Spawner {
 
     pub fn remove_entity(&mut self, entity: Entity) {
         self.entities_to_remove.push(entity);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::components::Name;
+
+    use super::*;
+
+    #[test]
+    fn test_add_entity() {
+        let mut spawner = Spawner::new();
+
+        spawner.add_entity(ComponentsPayload::from_name(Name::new(String::from("Sonic"))));
+        spawner.add_entity(ComponentsPayload::from_name(Name::new(String::from("Tails"))));
+
+        assert_eq!(spawner.entities_to_add.len(), 2);
+    }
+
+    fn test_remove_entity() {
+        let mut spawner = Spawner::new();
+
+        spawner.remove_entity(5);
+        spawner.remove_entity(7);
+
+        assert_eq!(spawner.entities_to_remove.len(), 2);
     }
 }
